4cb3b9e68e39df4fa94f53f01dc8ef4c3bee6332
richpk21 <richpk21@users.noreply.github.com>
2023-05-10 07:41:22 -0700

    add optimization-1
    9.x-optimization-1

---
+++ 9.x-optimization-1/rt.h
@@ -0,0 +1,24 @@
+#ifndef RT_H
+#define RT_H
+
+#include <cmath>
+#include <limits>
+#include <memory>
+#include <cstdlib>
+#include <random>
+
+#include "vec3.h"
+#include "ray.h"
+#include "color.h"
+#include "sphere.h"
+#include "camera.h"
+#include "hittable_list.h"
+#include "hittable.h"
+#include "material.h"
+#include "rtmath.h"
+#include "render.h"
+
+
+
+
+#endif
